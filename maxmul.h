0728d91fcb6209d12bdbd1e5364f6e848c3cb434
Md Mazder Rahman <p3sp4@unb.ca>
2023-02-27 03:22:22 -0400

    Update maxmul.h

--- maxmul.h
+++ maxmul.h
@@ -2,8 +2,6 @@
 #define MAXMUL_H_INCLUDED
 
 #include <stdlib.h>
-#include "mpi.h"
 
 void maxmul(int k, int m, int n, int A[m][k], int B[k][n], int C[m][n]);
-void pmaxmul(int k, int m, int n, int A[m][k], int B[k][n], int C[m][n], int P, int myrank);
 #endif // MAXMUL_H_INCLUDED
